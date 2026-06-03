35159d91a0cae9352c3f32e520d0a79221506f34
Neri Malinauskas <neri.malinauskas@protonmail.com>
2026-06-03 14:21:50 +0100

    Fix if data folder does not exist to create on the container.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 # If the /data volume is mounted onto the container, the volume is owned by the host and is inaccessible to the container.
 # The purpose of this entrypoint is to chown the host directory to something the container can access and update. 
 set -e
+mkdir -p /app/data
 chown -R app:app /app/data
 exec gosu app "$@"
