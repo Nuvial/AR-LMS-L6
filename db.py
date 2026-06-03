c66b80314126340c9018773c59571576a58c6365
Neri Malinauskas <neri.malinauskas@protonmail.com>
2026-06-03 09:31:25 +0100

    Improved db existence check and data structure to allow database persistence in containers

--- db.py
+++ db.py
@@ -3,6 +3,6 @@ from flask import g
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect('database.db')
+        g.db = sqlite3.connect('data/database.db')
         g.db.row_factory = sqlite3.Row
     return g.db
