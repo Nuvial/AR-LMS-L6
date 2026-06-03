b1b3364b84565563b5b2681737d990d5cc1849c3
Neri Malinauskas <neri.malinauskas@protonmail.com>
2026-06-03 13:58:35 +0100

    Fix binded volume "/data" from permission mismatch causing an exception to be raised when attempting to create a database.db file.

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# If the /data volume is mounted onto the container, the volume is owned by the host and is inaccessible to the container.
+# The purpose of this entrypoint is to chown the host directory to something the container can access and update. 
+set -e
+chown -R app:app /app/data
+exec gosu app "$@"
