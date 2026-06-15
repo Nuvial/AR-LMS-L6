3581016700d82e6eca3d08079fe245cfd25922ba
Neri Malinauskas <neri.malinauskas@protonmail.com>
2026-06-15 21:38:27 +0100

    Improved reusability and modulrisation of commonly used HTML elements.
    Moved `addFeedback` to helpers to improve reusability.

--- static/js/users/users.js
+++ static/js/users/users.js
@@ -395,15 +395,15 @@ function placeholderRow(){
     createInputFields(placeholderRow, 'sm', true, true)
 }
 
-function addFeedback(element, feedback, input){
-    $(element).text(feedback);
-    $(input).addClass('is-invalid');
-    return false;
-}
-
 //Validates initial employee ID and Username fields
 async function validateFields() {
     const promises = [];
+
+    function invalidate(feedbackDiv, feedback, input){
+        addFeedback(feedbackDiv, feedback, input);
+        return false;
+    }
+
     $('#newUserForm input').each(function(index, input) {
         const name = input.name;
         const value = $(input).val();
@@ -485,20 +485,6 @@ $('#createAccount').on('click', function(){
     placeholderRow();
 });
 
-// Temp password validation event handler
-$('#tempPassword').on('input', function(){
-    const feedback_div = $(this).siblings().closest('.invalid-feedback');
-    const val = $(this).val();
-
-    if (!(val.length > 3)){
-        addFeedback(feedback_div, 'Password must be greater than 3 characters', this);
-        $('#confirmPassword').addClass('disabled');
-    } else {
-        $('.is-invalid').removeClass('is-invalid');
-        $('#confirmPassword').removeClass('disabled');
-    }
-});
-
 //Event handler for form submission
 $('#confirmAccountBtn').on('click', function(){
     const form = $('#newUserForm');
@@ -515,14 +501,22 @@ $('#confirmAccountBtn').on('click', function(){
 //Delete action event handler
 $('#userTableBody').on('click', '.actions .fas.fa-trash', function(){
     const row = $(this).closest('tr');
-    const modal = $('#confirmDelete');
+    const modal = $('#deleteWarning');
     const employee_id = row.find('.employee-id-div').text();
-    const user_id = row.find('.user-id').text().trim();
+    const user_id = row.data('user-id');
     const username = row.find('.username-div').text();
 
-    modal.find('.user-id').text(user_id);
-    modal.find('.employee-id').text(employee_id);
-    modal.find('.username').text(username);
+    modal.find('.modal-title').text('Delete Account?')
+    modal.find('.modal-body').empty().append(
+        `
+        User ID: <span class="user-id">${user_id}</span>
+        <br>
+        Employee ID: <span class="employee-id">${employee_id}</span>
+        <br>
+        Username: <span class="username">${username}</span>
+        `
+    );
+
     modal.modal('toggle');
 });
 $('#confirmDeleteBtn').on('click', function(){
@@ -534,7 +528,7 @@ $('#userTableBody').on('click', '.actions .fas.fa-crown', function(){
     const row = $(this).closest('tr');
     const modal = $('#confirmPromote');
     const employee_id = row.find('.employee-id-div').text();
-    const user_id = row.find('.user-id').text().trim();
+    const user_id = row.data('user-id');
     const username = row.find('.username-div').text();
 
     modal.find('.user-id').text(user_id);
@@ -566,7 +560,7 @@ $('#confirmDemoteBtn').on('click', function(){
 //Change password action event handler
 $('#userTableBody').on('click', '.actions .fas.fa-key', function(){
     $('#confirmPassword').attr('data-bs-target', '#confirmPasswordChange').attr('data-bs-toggle', 'modal');
-    $('#hidden-user-id').val($(this).closest('tr').find('.user-id').text().trim());
+    $('#hidden-user-id').val($(this).closest('tr').data('user-id'));
     $('#tempPassword').val('');
     $('#passwordModal').modal('toggle');
 });
